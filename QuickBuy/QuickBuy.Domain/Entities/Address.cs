a5b4f513f0d252806c0afd20ab145787bb79da63
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2023-12-25 22:32:15 -0300

    Create Address entity

---
+++ QuickBuy/QuickBuy.Domain/Entities/Address.cs
@@ -0,0 +1,42 @@
+namespace QuickBuy.Domain.Entities
+{
+    public class Address : Entity
+    {
+        public int Id { get; set; }
+
+        public string ZipCode { get; set; } = string.Empty;
+
+		public string City { get; set; } = string.Empty;
+
+		public string State { get; set; } = string.Empty;
+
+		public string Street { get; set; } = string.Empty;
+
+		public int Number { get; set; }
+
+        public override void Validate()
+        {
+            ClearErrors();
+
+            if (string.IsNullOrEmpty(ZipCode))
+            {
+                AddErrorMessage("É preciso informar o CEP");
+            }
+
+            if (string.IsNullOrEmpty(City))
+            {
+                AddErrorMessage("É preciso informar a cidade");
+            }
+
+            if (string.IsNullOrEmpty(State))
+            {
+                AddErrorMessage("É preciso informar o estado");
+            }
+
+            if (string.IsNullOrEmpty(Street))
+            {
+                AddErrorMessage("É preciso informar a rua");
+            }
+        }
+    }
+}
